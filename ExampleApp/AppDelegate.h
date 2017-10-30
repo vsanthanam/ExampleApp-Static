7825de1777ac21d336e55b4682a213ccf35c550d
Varun Santhanam <varun.santhanam@sherbit.io>
2017-10-30 16:07:23 -0700

    Initial Commit

---
+++ ExampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleApp
+//
+//  Created by Varun Santhanam on 10/30/17.
+//  Copyright © 2017 Varun Santhanam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExampleApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ExampleApp
+//
+//  Created by Varun Santhanam on 10/30/17.
+//  Copyright © 2017 Varun Santhanam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
